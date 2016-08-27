3f0a06cb2f7f0ab601a9909719ad5386bb2fabb0
DELL <DELL@DELL-PC>
2016-08-27 16:45:27 +0500

    2nd Commit By Ayaz

---
+++ Reservations/Reservations/Controllers/DataBaseController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Reservations.Models;
+
+namespace Reservations.Controllers
+{
+    public class DataBaseController : ApiController
+    {
+        // GET: api/DataBase
+        public string GET(GenericModel[] data)
+        {
+            return "data";
+        }
+
+        // POST: api/DataBase
+        [HttpPost]
+        public string Post([FromBody]List<GenericModel> data)
+        {
+            return data.Count.ToString();
+        }
+
+        // PUT: api/DataBase/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/DataBase/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Reservations/Reservations/Scripts/CommonScripts/AjaxHandler.js
@@ -0,0 +1,82 @@
+﻿function ExecuteQueryCommand(param, proc) {
+    return $.ajax({
+        url: "api/DataBase/",
+        type: "POST",
+        datatype: "json",
+        data: JSON.stringify(param),
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            console.log(data);
+        },
+        async: true,
+        error: errorFn
+    });
+}
+function ExecuteNonQueryCommand(param, proc) {
+    return $.ajax({
+        url: "api/DataBase/",
+        type: "POST",
+        datatype: "json",
+        data: JSON.stringify({
+            "data": param,
+            "proc": proc
+        }),
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+        },
+        error: errorFn
+    });
+}
+function ExecuteNonQueryCommandWithOUTPUT(param, proc) {
+    return $.ajax({
+        url: "api/DataBase/",
+        type: "POST",
+        datatype: "json",
+        data: JSON.stringify({
+            "data": param,
+            "proc": proc
+        }),
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+        },
+        error: errorFn
+    });
+}
+
+
+function UserLogin(param, proc) {
+    return $.ajax({
+        url: "../../../UserLogin.aspx/Login",
+        type: "POST",
+        datatype: "json",
+        data: JSON.stringify({
+            "data": param,
+            "proc": proc
+        }),
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+
+        },
+        error: errorFn
+    });
+}
+
+function UserLogout() {
+    return $.ajax({
+        url: "../../../UserLogin.aspx/LogOut",
+        type: "POST",
+        datatype: "json",
+        contentType: "application/json; charset=utf-8",
+        success: function (data) {
+            if (data.d === "success") {
+                window.location.replace("../../../../../../UserLogin.aspx");
+            }
+        },
+        error: errorFn
+    });
+}
+
+function errorFn(err, status, xhr) {
+    alert(status);
+    $(".accordion-list-loader").hide();
+}
